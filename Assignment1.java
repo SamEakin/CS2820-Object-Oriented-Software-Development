1ce58717ff829a247e10d76999d0ea61a4f5e577
Sam Eakin <sseakin@gmail.com>
2017-01-27 16:09:02 -0600

    Initializing repo

---
+++ Assignment1.java
@@ -0,0 +1,19 @@
+/** Assignment1.java -- Recursive Example Problem
+ * @author Sam Eakin
+ * @version 2017-01-24
+ */
+public class Assignment1 {
+
+	public static int recurse(int i) {
+		if (i<3) {
+			return i;
+		}
+		return recurse(i-1) + recurse(i-3);
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++){
+			System.out.println("f("+i+")="+recurse(i));
+		}
+	}
+}
