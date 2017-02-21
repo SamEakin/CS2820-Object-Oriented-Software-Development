ecba3da5a189b43801cf62373f9dd1c30cd9b543
Sam Eakin <sseakin@gmail.com>
2017-02-20 21:48:52 -0600

    Pretty much every error handler works. Just need to add comments.

--- machineproblems/2/TernaryLogic.java
+++ machineproblems/2/TernaryLogic.java
@@ -88,16 +88,21 @@ class TernaryLogic {
 	public static void wireFactory(Scanner sc, LinkedList <Gate> gates) {
 		String inputSrc = sc.next();
 		String inputDst = sc.next();
+
+		if (!sc.hasNextFloat()) {
+			Errors.warn("Wire "+inputSrc+" "+inputDst+" delay expected.");
+			sc.nextLine();
+			return;
+		}
+
 		float inputDelay = sc.nextFloat();
 
 		if (findGate(inputSrc) == null) {
 			Errors.warn("Gate "+inputSrc+" undefined.");
 		}
-		if (findGate(inputDst) == null) {
+		else if (findGate(inputDst) == null) {
 			Errors.warn("Gate "+inputDst+" undefined.");
 		}
-
-		//@TODO: check for incorrect input data types
 
 		else {
 			sc.nextLine();
@@ -105,18 +110,42 @@ class TernaryLogic {
 		}
 	}
 
+	public static boolean isValidType(String inputType) {
+		String[] validTypes = {"min","max","neg","isTrue","isFalse","isUnknown"};
+		for(String type: validTypes) {
+			if (type.equals(inputType)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	// Factory method checks for correct inputs and calls a constructor if it passes
 	public static void gateFactory(Scanner sc, LinkedList <Gate> gates) {
 		String inputName = sc.next();
 		String inputType = sc.next();
+		//@TODO: add more conditionals to check for incorrect input data types
+		if (!isValidType(inputType)) {
+			Errors.warn("Gate "+inputName+" "+inputType+" is invalid gate type.");
+			sc.nextLine();
+		}
+
+		if (!sc.hasNextInt()) {
+			Errors.warn("Gate "+inputName+" "+inputType+" inputs expected.");
+			sc.nextLine();
+			return;
+		}
 		int inputInputs = sc.nextInt();
+		if (!sc.hasNextFloat()) {
+			Errors.warn("Gate "+inputName+" "+inputType+" delay expected.");
+			sc.nextLine();
+			return;
+		}
 		float inputDelay = sc.nextFloat();
 
 		if (findGate(inputName) != null) {
 			Errors.warn("Gate "+inputName+" already defined.");
 		}
-
-		//@TODO: add more conditionals to check for incorrect input data types
 
 		else {
 			sc.nextLine();
